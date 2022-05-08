5463cc32215b400fe9a3ff0d53e3a78e8de91eae
Antony Caseres <accasere@outlook.com>
2022-05-08 01:58:23 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// Adding Event Listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("The DOM has loaded")
+    document.querySelector('#text').innerHTML = 'This is really cool!'
+    
+})
+
